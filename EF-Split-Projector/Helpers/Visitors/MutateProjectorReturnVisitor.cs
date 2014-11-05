4805cc2e06a31dfcddba1ecdef2da75b4aa14338
rissaman <romanissa@hotmail.com>
2014-11-05 12:48:28 -0700

    Added ProjectorMeger.Merge with methods.
    Methods allow merging of projectors with related parameters or return
    types.

---
+++ EF-Split-Projector/Helpers/Visitors/MutateProjectorReturnVisitor.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Linq.Expressions;
+using EF_Split_Projector.Helpers.Extensions;
+
+namespace EF_Split_Projector.Helpers.Visitors
+{
+    internal class MutateProjectorReturnVisitor : ExpressionVisitor
+    {
+        /// <summary>
+        /// Returns a new projector resulting in a different return type (which must be derived from the projector's original return type, or else be the same type).
+        /// </summary>
+        internal static Expression<Func<TSource, TDestDerived>> Mutate<TSource, TDestBase, TDestDerived>(Expression<Func<TSource, TDestBase>> projector)
+            where TDestBase : new()
+            where TDestDerived : TDestBase, new()
+        {
+            if(!projector.IsProjector()) { throw new ArgumentException("Expression is not projector.", "projector"); }
+
+            return new MutateProjectorReturnVisitor().MutateReturn<TSource, TDestBase, TDestDerived>(projector);
+        }
+
+        #region Private Parts
+
+        private Type _newType;
+        private NewExpression _oldNewExpression;
+
+        private Expression<Func<TSource, TDestDerived>> MutateReturn<TSource, TDestBase, TDestDerived>(Expression<Func<TSource, TDestBase>> expression)
+            where TDestBase : new()
+            where TDestDerived : TDestBase, new()
+        {
+            _newType = typeof(TDestDerived);
+
+            if(typeof(TDestBase) == _newType)
+            {
+                return Expression.Lambda<Func<TSource, TDestDerived>>(expression.Body, expression.Parameters);
+            }
+
+            _oldNewExpression = ((MemberInitExpression)expression.Body).NewExpression;
+
+            var visitedExpression = (LambdaExpression)Visit(expression);
+            return Expression.Lambda<Func<TSource, TDestDerived>>(visitedExpression.Body, visitedExpression.Parameters);
+        }
+
+        protected override Expression VisitNew(NewExpression node)
+        {
+            return node == _oldNewExpression ? Expression.New(_newType) : base.VisitNew(node);
+        }
+
+        #endregion
+    }
+}
--- EF-Split-Projector/ProjectorMerge.cs
+++ EF-Split-Projector/ProjectorMerge.cs
@@ -8,6 +8,67 @@ namespace EF_Split_Projector
 {
     public static class ProjectorMerge
     {
+        /// <summary>
+        /// <para>Will merge a projector that results in a base type with a projector that results in a derived type such that</para>
+        /// <para>given CustomerResult : PersonResult, then</para>
+        /// <para>customer => new PersonResult { Name = customer.Name }</para>
+        /// <para>merged with:</para>
+        /// <para>customer => new CustomerResult { CustomerId = customer.Id }</para>
+        /// <para>will result in:</para>
+        /// <para>customer => new CustomerResult { Name = customer.Name, CustomerId = customer.Id }</para>
+        /// </summary>
+        /// <typeparam name="TSource">The input type for the projectors.</typeparam>
+        /// <typeparam name="TDestBase">The return type for the baseProjector.</typeparam>
+        /// <typeparam name="TDestDerived">The return type for the derivedProjector. Note that this must either be the same as TDestBase or else derive from it.</typeparam>
+        /// <param name="baseProjector">The base projector.</param>
+        /// <param name="derivedProjector">The derived projector that will be based on the base projector.</param>
+        /// <returns>A new expression tree representing the merged projection between baseProjector and derivedProjector.</returns>
+        public static Expression<Func<TSource, TDestDerived>> Merge<TSource, TDestBase, TDestDerived>(this Expression<Func<TSource, TDestBase>> baseProjector, Expression<Func<TSource, TDestDerived>> derivedProjector)
+            where TDestBase : new()
+            where TDestDerived : TDestBase, new()
+        {
+            if(baseProjector == null) { throw new ArgumentNullException("baseProjector"); }
+            if(derivedProjector == null) { throw new ArgumentNullException("baseProjector"); }
+
+            var mutatedBase = MutateProjectorReturnVisitor.Mutate<TSource, TDestBase, TDestDerived>(baseProjector);
+            return new[] { mutatedBase, derivedProjector }.Merge();
+            //return MergeProjectorsVisitor.Merge(mutatedBase, derivedProjector);
+        }
+
+        /// <summary>
+        /// <para>Will merge a projector that results in a base type with a projector that results in a derived type when both projectors have differing but related input types such that</para>
+        /// <para>given CustomerResult : PersonResult, then</para>
+        /// <para>person => new PersonResult { Name = person.Name }</para>
+        /// <para>merged with:</para>
+        /// <para>customer => new CustomerResult { CustomerId = customer.Id }</para>
+        /// <para>defining:</para>
+        /// <para>customer => customer.person</para>
+        /// <para>will result in:</para>
+        /// <para>customer => new CustomerResult { Name = customer.person.Name, CustomerId = customer.Id }</para>
+        /// </summary>
+        /// <typeparam name="TBaseSource">The input type for the base projector.</typeparam>
+        /// <typeparam name="TDerivedSource">The input type for the derived projector. Note that TNewSource does not have to derive from TOldSource, only be able to somehow result in it.</typeparam>
+        /// <typeparam name="TBaseResult">The return type for the base projector.</typeparam>
+        /// <typeparam name="TDerivedResult">The return type for the derivedProjector. Note that this must either be the same as TDestBase or else derive from it.</typeparam>
+        /// <param name="baseProjector">The base projector.</param>
+        /// <param name="derivedProjector">The derived projector that will be based on the base projector.</param>
+        /// <param name="oldSourceSelector">An expression defining how to get from TNewSource to TOldSource.</param>
+        /// <returns>A new expression tree representing the merged projection between baseProjector with a new input type and derivedProjector.</returns>
+        public static Expression<Func<TDerivedSource, TDerivedResult>> Merge<TBaseSource, TDerivedSource, TBaseResult, TDerivedResult>(
+            this Expression<Func<TBaseSource, TBaseResult>> baseProjector,
+            Expression<Func<TDerivedSource, TDerivedResult>> derivedProjector,
+            Expression<Func<TDerivedSource, TBaseSource>> oldSourceSelector)
+            where TBaseResult : new()
+            where TDerivedResult : TBaseResult, new()
+        {
+            if(baseProjector == null) { throw new ArgumentNullException("baseProjector"); }
+            if(derivedProjector == null) { throw new ArgumentNullException("baseProjector"); }
+            if(oldSourceSelector == null) { throw new ArgumentNullException("oldSourceSelector"); }
+
+            var mutatedSource = MutateProjectorSingleParameterVisitor.Mutate(baseProjector, oldSourceSelector);
+            return mutatedSource.Merge(derivedProjector);
+        }
+
         public static Expression<Func<TSource, TDest>> Merge<TSource, TDest>(this IEnumerable<Expression<Func<TSource, TDest>>> projectors)
         {
             return projectors == null ? null : MergeOnProjectorVisitor.Merge(projectors.ToArray());
