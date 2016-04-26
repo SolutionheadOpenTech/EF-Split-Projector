2c7825bfebe9807e8796704d856b463e4500a167
rissaman <romanissa@hotmail.com>
2016-04-26 17:13:42 -0600

    SHLIB-3: Attempting to prevent duplicate type exception.
    Modified UniqueTypeBuilder to differentiate between types using thread
    ID.

--- Tests/UniqueTypeBuilderTests.cs
+++ Tests/UniqueTypeBuilderTests.cs
@@ -29,29 +29,5 @@ namespace Tests
             var result = UniqueTypeBuilder.GetUniqueType(members2, null);
             Assert.AreEqual(expected, result);
         }
-
-        [Test]
-        public void Returns_same_type_if_cache_is_cleared()
-        {
-            var members = new Dictionary<string, Type>
-                {
-                    { "someint", typeof(int) },
-                    { "someOtherint", typeof(int) },
-                    { "someString", typeof(int) },
-                };
-            var members2 = new Dictionary<string, Type>
-                {
-                    { "someString", typeof(int) },
-                    { "someOtherint", typeof(int) },
-                    { "someint", typeof(int) },
-                };
-
-            var expected = UniqueTypeBuilder.GetUniqueType(members, null);
-            var cache = typeof(UniqueTypeBuilder).GetField("DynamicTypes", BindingFlags.Static | BindingFlags.Public | BindingFlags.NonPublic).GetValue(null);
-            cache.GetType().GetMethod("Clear").Invoke(cache, new object[] { });
-
-            var result = UniqueTypeBuilder.GetUniqueType(members2, null);
-            Assert.AreEqual(expected, result);
-        }
     }
 }
